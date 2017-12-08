9388e542d33190519330856494c251f73e818a0b
danieljardine <1005730@student.sae.edu.au>
2017-12-08 18:32:44 +1000

    Mechanics
    Player movement changes, gold pickups and effects, enemy, player health and damage, knockback, player invincibility when necessary, respawning and respawning effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveSpeed, jumpForce, gravityScale;
+    public float moveSpeed, jumpForce, gravityScale, rotateSpeed, knockbackForce, knockbackTime;
+    private float KnockbackCounter;
     public CharacterController controller;
 
     private Vector3 moveDirection;
 
     public Animator anim;
+    public Transform pivot;
+    public GameObject playerModel;
 
 	// Use this for initialization
 	void Start () {
@@ -19,24 +22,48 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
-        float yStore = moveDirection.y;
-        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
-        moveDirection = moveDirection.normalized * moveSpeed;
-        moveDirection.y = yStore;
-
-        if (controller.isGrounded)
+        if (KnockbackCounter <= 0)
         {
-            moveDirection.y = 0f;
-            if (Input.GetButtonDown("Jump"))
+            float yStore = moveDirection.y;
+            moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+            moveDirection = moveDirection.normalized * moveSpeed;
+            moveDirection.y = yStore;
+
+            if (controller.isGrounded)
             {
-                moveDirection.y = jumpForce;
+                moveDirection.y = 0f;
+                if (Input.GetButtonDown("Jump"))
+                {
+                    moveDirection.y = jumpForce;
+                }
             }
+        }
+        else
+        {
+            KnockbackCounter -= Time.deltaTime;
         }
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
         controller.Move(moveDirection * Time.deltaTime);
 
+        //Move the player in different directions based on camera look direction
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
         anim.SetBool("isGrounded", controller.isGrounded);
         anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
+    }
+
+    public void Knockback(Vector3 direction)
+    {
+        KnockbackCounter = knockbackTime;
+
+        direction = new Vector3(1f, 1f, 1f);
+
+        moveDirection = direction * knockbackForce;
+        moveDirection.y = knockbackForce;
     }
 }
